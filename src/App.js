717a8f325fd22a0e1e2ecc5065c6ab14220af98b
maryamrmz <maryam.rpour@gmail.com>
2020-09-23 13:15:45 +0330

    Reset the last commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import Basket from "./pages/basket/basket.component";
 
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
+
 function App() {
+    useEffect(() => {
+        return () => {
+            // Init Materialize JS
+            M.AutoInit();
+        };
+    }, []);
+
     return (
         <div>
             <Header />
@@ -17,3 +28,4 @@ function App() {
 }
 
 export default App;
+
